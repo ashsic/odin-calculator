f77e6279e113f51a406ba4aaea24c9d67215f59f
ashsic <99445200+ashsic@users.noreply.github.com>
2023-11-07 09:40:49 -0500

    beginning work on JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 // Main.js
+
+// Targeting
+let textBox = document.querySelector("#display-text");
+let digits = document.querySelector("#digits").querySelectorAll("button");
+
+
+// Click handlers
+function handleDigitClick(event){
+    if (event.target.textContent === "0" && textBox.textContent === ""){}
+    else textBox.textContent += event.target.textContent;
+};
+
+
+
+
+
+
+
+
+
+
+
+digits.forEach(digit=>{
+    digit.addEventListener("click", handleDigitClick)
+});
