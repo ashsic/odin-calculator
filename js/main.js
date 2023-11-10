90fa4702fb81fb42e2b74e5c2bb4a29b78988a43
ashsic <99445200+ashsic@users.noreply.github.com>
2023-11-10 14:31:57 -0500

    adding JS functionality to button clicks

--- js/main.js
+++ js/main.js
@@ -2,25 +2,34 @@
 
 // Targeting
 let textBox = document.querySelector("#display-text");
-let digits = document.querySelector("#digits").querySelectorAll("button");
+let buttons = document.querySelectorAll("button");
+
+function handleButtonClick(event) {
+    let ettc = "";
+    if (event.type === "click") {
+        ettc = event.target.textContent;
+    }
+
+    if (ettc >= '0' && ettc <= '9') {
+        if (event.target.textContent === "0" && textBox.textContent === ""){}
+        else textBox.textContent += event.target.textContent;
+    } else if (ettc === 'AC') {
+        textBox.textContent = "";
+    } else if (ettc === '+/-') {
+        if (textBox.textContent.charAt(0) === '-') {
+            textBox.textContent = textBox.textContent.slice(1);
+        } else {
+            textBox.textContent = "-" + textBox.textContent;
+        }
+    } else if (ettc === '%') {
+        textBox.textContent = parseFloat(textBox.textContent) / 100.0;
+    } else if ("/x-+".includes(ettc)) {
+        
+    }
+}
 
 
-// Click handlers
-function handleDigitClick(event){
-    if (event.target.textContent === "0" && textBox.textContent === ""){}
-    else textBox.textContent += event.target.textContent;
-};
 
-
-
-
-
-
-
-
-
-
-
-digits.forEach(digit=>{
-    digit.addEventListener("click", handleDigitClick)
+buttons.forEach(button => {
+    button.addEventListener("click", handleButtonClick)
 });
