92ffc0b429d8b205a8712d53715b5b94f18a27a4
ashsic <99445200+ashsic@users.noreply.github.com>
2023-11-16 20:47:35 -0500

    Added calculator object in function

--- js/main.js
+++ js/main.js
@@ -46,5 +46,19 @@ buttons.forEach(button => {
 });
 
 function calculateResult(num1, num2, operator) {
-    let calculator = {}
+    let calculator = {
+        add: (num1, num2) => {
+            return num1 + num2;
+        },
+        subtract: (num1, num2) => {
+            return num1 - num2;
+        },
+        multiply: (num1, num2) => {
+            return num1 * num2;
+        },
+        divide: (num1, num2) => {
+            return num1 / num2;
+        }
+
+    }
 }
